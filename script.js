16637588ec2cb40a9abedfa30d60aca7e0d2fe9f
thunderamental <48991065+thunderamental@users.noreply.github.com>
2020-01-13 14:56:23 +1100

    Fixed generating elements and added preorder index array, fixed circular JSON stringify
    Generating elements now are correct. Elements now include a leaf pre-order array for later use. The JSON.stringify error on circular objects has been circled around (haha).
    
    Next up is to implement a subtree copier, if it doesnt make sense to just naively do it.

--- script.js
+++ script.js
@@ -39,9 +39,10 @@ function renderTwo(){
 console.log("Javascript test of Thompson F element operation");
 
 class caretNode {
-    constructor(value) {
+    constructor(input) {
         this.left = null;
         this.right = null;
+        this.parent = input;
     }
 }
 
@@ -49,27 +50,80 @@ class eltF {
     constructor(inA, inB) {
         this.a = inA;
         this.b = inB;
+        this.leafA = []; // this holds the tree's leaves in preorder form. 
+        this.leafB = [];
     }
 }
 
-/* the following are the two generating elements for F. */
-var gA = new eltF(new caretNode(), new caretNode());
-gA.a.left = new caretNode();
-gA.b.right = new caretNode();
+function generatePreorder(node, outputArray) {
+  if (node.left == null) {
+    outputArray.push(node);
+  } else {
+    generatePreorder(node.left, outputArray);
+    generatePreorder(node.right, outputArray);
+  }
+}
 
-var gB = new eltF(new caretNode(), new caretNode());
-gB.a.left = new caretNode();
-gB.a.left.left = new caretNode();
-gB.b.right = new caretNode();
-gB.b.right.right = new caretNode();
+function getPreorderIndex(node, preorder) {
+  return preorder.indexOf(node);
+
+  for (leaf in preorder) {
+    if (node === leaf) { return preorder.indexOf(node); }
+  } // this entire function is INCREDIBLY unnecessary but i'm keeping it as a sign LOL
+}
+
+/* the following hardcodes the two generating elements for F. */
+var gA = new eltF(new caretNode(null), new caretNode(null));
+gA.a.left = new caretNode(gA.a);
+gA.a.right = new caretNode(gA.a);
+gA.a.left.left = new caretNode(gA.a.left);
+gA.a.left.right = new caretNode(gA.a.left);
+gA.b.right = new caretNode(gA.b);
+gA.b.left = new caretNode(gA.b);
+gA.b.right.left = new caretNode(gA.b.right);
+gA.b.right.right = new caretNode(gA.b.right);
+generatePreorder(gA.a, gA.leafA);
+generatePreorder(gA.b, gA.leafB);
 
 console.log("The first generating element:", gA);
+console.log("A preorder on the references to leaves is included:")
+console.log("The preorder index of gA.a.left.left is " + gA.leafA.indexOf(gA.a.left.left));
+console.log("The preorder index of gA.a.left.right is " + gA.leafA.indexOf(gA.a.left.right));
+
+var gB = new eltF(new caretNode(null), new caretNode(null));
+gB.a.left = new caretNode(gB.a);
+gB.a.right = new caretNode(gB.a);
+gB.a.right.left = new caretNode(gB.a.right);
+gB.a.right.left.left = new caretNode(gB.a.right.left);
+gB.a.right.left.right = new caretNode(gB.a.right.left);
+gB.a.right.right = new caretNode(gB.a.right);
+gB.b.left = new caretNode(gB.b);
+gB.b.right = new caretNode(gB.b);
+gB.b.right.left = new caretNode(gB.b.right);
+gB.b.right.right = new caretNode(gB.b.right);
+gB.b.right.right.left = new caretNode(gB.b.right.right);
+gB.b.right.right.right = new caretNode(gB.b.right.right);
+generatePreorder(gB.a, gB.leafA);
+generatePreorder(gB.b, gB.leafB);
+
 
 console.log("The second generating element:", gB);
 
 function copyNode(inp) {
-    let clone = JSON.parse(JSON.stringify(inp));
-    return clone;
+  var cache = [];
+    let clone = JSON.parse(JSON.stringify(inp, function(key, value) {
+      if (typeof value === 'object' && value !== null) {
+          if (cache.indexOf(value) !== -1) {
+              // Duplicate reference found, discard key
+              return;
+          }
+          // Store value in our collection
+          cache.push(value);
+      }
+      return value;
+  }));
+  cache = null;
+  return clone;
 }
 
 function fOperation(first, second) {
@@ -117,7 +171,7 @@ function fOperation(first, second) {
     var output = new eltF(out1, out2);
 
     return output;
-
+ 
 }
 
 console.log("Output of gA*gA: ", fOperation(gA, gA));
